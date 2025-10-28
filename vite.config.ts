9939e43b5cb2ed111a7f13b6286a5553b28372c0
Dr Alexander MurMax <garyerneus@gmail.com>
2025-10-27 20:03:49 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // optional: for local development
-  },
-  build: {
-    outDir: "dist", // ensures Vercel serves the correct folder
-  },
+  build: { outDir: "dist" },
 });
